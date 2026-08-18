9a0518925d3615c3ffcce61aae74b9e3ada12b65
Kartik Hedgotre <kartikhedgotre50@gmail.com>
2026-08-18 22:26:05 +0530

    Improve the smooth scrolling effect

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -4,20 +4,29 @@ import Lenis from "lenis";
 export default function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.4,
+      lerp: 0.08,
       smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
+      wheelMultiplier: 0.8,
+      touchMultiplier: 1.5,
+      infinite: false,
+      autoRaf: false,
+      syncTouch: true,
+      syncTouchLerp: 0.075,
+      easing: (t) => 1 - Math.pow(1 - t, 4),
     });
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+    let animationFrameId;
 
-    requestAnimationFrame(raf);
+    const raf = (time) => {
+      lenis.raf(time);
+      animationFrameId = requestAnimationFrame(raf);
+    };
+
+    animationFrameId = requestAnimationFrame(raf);
 
     return () => {
+      cancelAnimationFrame(animationFrameId);
       lenis.destroy();
     };
   }, []);
