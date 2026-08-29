bfb134513a3ce3e0284c82b82015231f78fd4a49
Kartik Hedgotre <kartikhedgotre50@gmail.com>
2026-08-29 13:03:59 +0530

    fix the errors in smoothScroll

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -4,16 +4,16 @@ import Lenis from "lenis";
 export default function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.4,
-      lerp: 0.08,
+      duration: 0.8,
+      lerp: 0.12,
       smoothWheel: true,
-      wheelMultiplier: 0.8,
+      wheelMultiplier: 1,
       touchMultiplier: 1.5,
       infinite: false,
       autoRaf: false,
       syncTouch: true,
-      syncTouchLerp: 0.075,
-      easing: (t) => 1 - Math.pow(1 - t, 4),
+      syncTouchLerp: 0.1,
+      easing: (t) => 1 - Math.pow(1 - t, 3),
     });
 
     let animationFrameId;
